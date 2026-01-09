2c02859ec678b043904f899d42bb495e5d712ccc
sujith <suji4073@gmail.com>
2026-01-09 10:54:59 +0530

    latest update

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with base URL
 const api = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_URL || 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_SERVER_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
